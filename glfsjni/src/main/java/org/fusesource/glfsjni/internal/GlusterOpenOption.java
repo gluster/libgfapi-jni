276f052ceb948023a7785e4ddbd0cadfaebe5f38
Louis Zuckerman <me@louiszuckerman.com>
2013-07-08 22:10:03 -0400

    changed access modifiers back to public

--- glfsjni/src/main/java/org/fusesource/glfsjni/internal/GlusterOpenOption.java
+++ glfsjni/src/main/java/org/fusesource/glfsjni/internal/GlusterOpenOption.java
@@ -16,23 +16,23 @@ import java.util.List;
 @ToString(of = "options", includeFieldNames = false)
 public class GlusterOpenOption {
     @JniField(flags = {FieldFlag.CONSTANT})
-    private static int O_APPEND;
+    public static int O_APPEND;
     @JniField(flags = {FieldFlag.CONSTANT})
-    private static int O_SYNC;
+    public static int O_SYNC;
     @JniField(flags = {FieldFlag.CONSTANT})
-    private static int O_DSYNC;
+    public static int O_DSYNC;
     @JniField(flags = {FieldFlag.CONSTANT})
-    private static int O_TRUNC;
+    public static int O_TRUNC;
     @JniField(flags = {FieldFlag.CONSTANT})
-    private static int O_RDONLY;
+    public static int O_RDONLY;
     @JniField(flags = {FieldFlag.CONSTANT})
-    private static int O_WRONLY;
+    public static int O_WRONLY;
     @JniField(flags = {FieldFlag.CONSTANT})
-    private static int O_RDWR;
+    public static int O_RDWR;
     @JniField(flags = {FieldFlag.CONSTANT})
-    private static int O_CREAT;
+    public static int O_CREAT;
     @JniField(flags = {FieldFlag.CONSTANT})
-    private static int O_EXCL;
+    public static int O_EXCL;
     @Getter
     private int value;
     private List<String> options = new LinkedList<String>();
